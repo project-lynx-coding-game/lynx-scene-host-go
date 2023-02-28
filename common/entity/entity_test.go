b8a148cee3426f51bc20e78aa2bf69385611299d
blazej-smorawski <blazej.smorawski@gmail.com>
2023-02-28 14:49:08 +0100

    Add test for `Entity`

--- common/entity/entity_test.go
+++ common/entity/entity_test.go
@@ -1,7 +1,29 @@
 package entity
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/group-project-gut/lynx-scene-host/common"
+)
 
 func TestEntitySerialization(t *testing.T) {
-	// TODO:
+	object := common.Object{Name: "Ondrejek"}
+	entity := Entity{&object}
+
+	expected := fmt.Sprintf(`{"type":"Object","args":"%s"}`, strings.ReplaceAll(object.Args(), `"`, `\"`))
+	serialized_entity, err := json.Marshal(entity)
+	if err != nil {
+		t.Fatalf(`Failed to serialize entity - %v`, err)
+	}
+
+	if expected != string(serialized_entity) {
+		t.Fatalf(`Serialization format is wrong - got: %s, expected: %s`, string(serialized_entity), expected)
+	}
+}
+
+func TestEntityDeserialization(t *testing.T) {
+	//TODO:
 }
