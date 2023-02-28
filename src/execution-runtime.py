71be2d72249b430868d842abea59c282113229d3
blazej-smorawski <blazej.smorawski@gmail.com>
2023-02-28 14:49:55 +0100

    Execution-runtime changes for testing

--- src/execution-runtime.py
+++ src/execution-runtime.py
@@ -4,5 +4,5 @@ from lynx.common.vector import Vector
 
 input = input()
 scene = Scene.deserialize(input)
-scene.add_entity(Move(object_id=0, vector=Vector(1,1)))
+scene.add_entity(Move(object_id=123, vector=Vector(1,1)))
 print(scene.serialize())
